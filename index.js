9c4eda3c08262bc88169c4004e0336382f019eba
psyitama <psyrone.agpalo@yahoo.com>
2021-10-04 16:16:12 +0800

    Adjust spacing

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ $(document).ready(function () {
 */
 function getID(url){
     url = url.split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+    
     return (url[2] !== undefined) ? url[2].split(/[^0-9a-z_\-]/i)[0] : url[0];
  }
 
